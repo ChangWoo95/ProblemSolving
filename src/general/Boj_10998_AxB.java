20e151931b37778127c1b15cb9d3c52fb4f22cd8
qaqa313a <qaqa313a@naver.com>
2021-01-01 22:10:00 +0900

    docs: Boj_10998_AxB solving

---
+++ src/general/Boj_10998_AxB.java
@@ -0,0 +1,14 @@
+package general;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_10998_AxB {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a * b);
+    }
+}
