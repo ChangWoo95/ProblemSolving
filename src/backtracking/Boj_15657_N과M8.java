da72c6d873059ecb50b552bb9124130e7676d2de
qaqa313a <qaqa313a@naver.com>
2021-01-24 20:46:17 +0900

    :art: docs: Boj_15657_N과M8 solving

---
+++ src/backtracking/Boj_15657_N과M8.java
@@ -0,0 +1,41 @@
+package backtracking;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_15657_N과M8 {
+    static int n, m, output[], input[];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        output = new int[m];
+        input = new int[n];
+
+        for(int i = 0; i< n; i++){
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
+
+        perm(0, 0);
+        System.out.print(sb);
+
+    }
+    static void perm(int depth, int start) {
+        if(depth == m) {
+            for(int i = 0 ; i < depth; i++){
+                sb.append(output[i]).append(" ");
+            }
+            sb.append("\n");
+            return ;
+        }
+        for(int i = start; i < input.length; i++){
+            output[depth] = input[i];
+            perm(depth + 1, i);
+        }
+    }
+}
