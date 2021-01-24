f174dec03ba4c82e09e1bfcbb3c497b89b1e5fab
qaqa313a <qaqa313a@naver.com>
2021-01-24 20:00:48 +0900

    :memo: docs: Boj_15650_N과M2 update

--- src/backtracking/Boj_15650_N과M2.java
+++ src/backtracking/Boj_15650_N과M2.java
@@ -4,8 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Boj_15650_N과M2 {
-    static int[] numbers, output; // numbers: 숫자들의 집합, output: m개씩 뽑았을 경우의 집합
-    static boolean[] visited; // 해당 요소를 방문했는지 확인
+    static int[] numbers; // numbers: 숫자들의 집합
     static int n, m; // n: 전체 숫자의 갯수, m: 그 중에 뽑을 숫자의 갯수
     static StringBuilder sb = new StringBuilder();
 
@@ -16,38 +15,24 @@ public class Boj_15650_N과M2 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         numbers = new int[n];
-        visited = new boolean[n];
-        output = new int[m];
 
         for(int i = 0; i<n ; i++){
             numbers[i] = i+1;
         }
-        perm(0);
+        perm(0, 1);
         System.out.print(sb);
     }
-    static void perm(int depth) {
+    static void perm(int depth, int start) {
         if(depth == m) { //m개만큼 뽑았다면
-            if(!isOrdered()) return; // 오름차순이 아니라면 탈출
-
-            for(int i = 0; i < output.length; i++){
-                sb.append(output[i]).append(" ");
+            for(int i = 0; i < depth; i++){
+                sb.append(numbers[i]).append(" ");
             }
             sb.append("\n");
             return ;
         }
-        for(int i = 0; i < numbers.length; i++){
-            if(!visited[i]){ // 해당 요소를 방문하지 않았다면,
-                visited[i] = true; // 방문
-                output[depth] = numbers[i]; // 해당 값을 output에 담음
-                perm(depth+1);
-                visited[i] = false;
-            }
+        for(int i = start; i <= n; i++){
+            numbers[depth] = i;
+            perm(depth+1, start+1);
         }
-    }
-    static boolean isOrdered(){
-        for(int i = 0; i < output.length-1; i++){
-            if(output[i] > output[i+1]) return false;
-        }
-        return true;
     }
 }
