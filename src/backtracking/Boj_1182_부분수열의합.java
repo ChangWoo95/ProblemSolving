4839dfaec446b6d6dfff2fa74ad71686bfeb157c
qaqa313a <qaqa313a@naver.com>
2021-02-08 02:05:07 +0900

    Boj_1182_부분수열의합 solving

---
+++ src/backtracking/Boj_1182_부분수열의합.java
@@ -0,0 +1,46 @@
+package backtracking;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_1182_부분수열의합 {
+    static int n, m, cnt = 0, ans = 0;
+    static int[] list;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        list = new int[n];
+        visited = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i =0; i <n; i++){
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+        func(0);
+        System.out.print(ans);
+    }
+    static void func(int depth) {
+        //if(cnt == 1 << (n - 1)) return ;
+        if(depth == n) {
+            long sum = 0;
+            boolean flag = false;
+            for(int i =0; i <n; i++){
+                if(visited[i]) {
+                    sum += list[i];
+                    flag = true;
+                }
+            }
+            if(flag && sum == m) ans++;
+            cnt++;
+            return;
+        }
+        visited[depth] = true;
+        func(depth + 1);
+        visited[depth] = false;
+        func(depth + 1);
+    }
+}
