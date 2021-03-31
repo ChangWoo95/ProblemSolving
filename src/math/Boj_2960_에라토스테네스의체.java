921419a1d0fab8cc0860562e2919080fd4d6f55b
qaqa313a <qaqa313a@naver.com>
2021-03-31 17:21:23 +0900

    :art: docs: Boj_2960_에라토스테네스의체 Solving

---
+++ src/math/Boj_2960_에라토스테네스의체.java
@@ -0,0 +1,30 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_2960_에라토스테네스의체 {
+    static boolean[] prime;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        prime = new boolean[n+1];
+        for (int i = 2; i <= n; ++i) {
+            for (int j = i; j <= n; j += i) {
+                if(prime[j]) continue;
+                prime[j] = true;
+                --k;
+                if(k == 0){
+                    System.out.println(j);
+                    System.exit(0);
+                }
+            }
+        }
+
+    }
+}
