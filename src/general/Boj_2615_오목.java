322ee196d5a426e28771bdd8c7f3455f73b9549f
qaqa313a <qaqa313a@naver.com>
2021-04-24 20:40:00 +0900

    :art: docs: Boj_2615_오목 Solving

---
+++ src/general/Boj_2615_오목.java
@@ -0,0 +1,44 @@
+package general;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_2615_오목 {
+    static int[][] map = new int[21][21];
+    static int[][][] memo = new int[21][21][4];
+    static int[] dx = { 1, 1, 0, -1 };
+    static int[] dy = { 0, 1, 1, 1 };
+
+    public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 1; i <= 19; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= 19; j++)
+                map[i][j] = Integer.parseInt(st.nextToken());
+        }
+        System.out.print(findFive());
+    }
+
+    static String findFive() {
+        for (int j = 1; j <= 19; j++) {
+            for (int i = 1; i <= 19; i++) {
+                if (map[i][j] != 0) {
+                    for (int d = 0; d < 4; d++) {
+                        if (memo[i][j][d] == 0 && calc(i, j, d, map[i][j]) == 5) {
+                            return map[i][j] + "\n" + i + " " + j + "\n";
+                        }
+                    }
+                }
+            }
+        }
+        return "0";
+    }
+
+    static int calc(int x, int y, int dir, int color) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if (map[nx][ny] == color)
+            return memo[nx][ny][dir] = calc(nx, ny, dir, color) + 1;
+        return 1;
+    }
+}
